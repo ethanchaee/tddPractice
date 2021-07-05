94b125849ac61826b6b2974df124c0d2d9a573dd
aschae <aschae@woowahan.com>
2021-07-06 00:12:27 +0900

    [chore] version sync

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,12 +15,14 @@ repositories {
 }
 
 dependencies {
+    implementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
+    testImplementation("io.kotest:kotest-assertions-core:4.6.0")
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-    testImplementation("io.kotest:kotest-framework-engine:4.6.2")
-    testImplementation("io.kotest:kotest-assertions-core:4.6.2")
-    testImplementation("io.kotest:kotest-property:4.6.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-params:5.6.0")
+    testImplementation("io.kotest:kotest-framework-engine:4.6.0")
+    testImplementation("io.kotest:kotest-property:4.6.0")
 }
 
 tasks.test {
---
+++ src/test/kotlin/StockReport.kt
@@ -0,0 +1,9 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+internal class StockReport : FunSpec({
+
+    test("환율 계산하기") {
+        1 + 2 shouldBe 3
+    }
+})
